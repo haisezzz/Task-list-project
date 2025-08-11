913b38829bf8e1c6d20017f794df313de4dc56ea
Inesa Uskeviciute (ZBC24INUS) <haisechii@gmail.com>
2025-08-11 09:47:27 +0200

    base program
    added the following:
    - loop for running the program
    - menu for options
    - if/else to handle input error for menu
    - switch to call methods
    - option to close the program
    - temporal methods to check if calling works

--- Task list project/Program.cs
+++ Task list project/Program.cs
@@ -2,6 +2,87 @@
 {
   static void Main(string[] args)
   {
+    string[] tasksList = new string[4];
+    
+    bool isRunning = true;
+    while(isRunning)
+    {
+      Console.Clear();
+      Console.WriteLine("Welcome to the What Should My Husband Do Today app!");
+      Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - -");
+      Console.WriteLine("Please select an optiom:");
+      Console.WriteLine("1. Give him a new task");
+      Console.WriteLine("2. Remove a task");
+      Console.WriteLine("3. Mark a task as done");
+      Console.WriteLine("4. Show all tasks");
+      Console.WriteLine("5. Exit the app");
+      Console.WriteLine();
+      string? input = Console.ReadLine();
+      int.TryParse(input, out int choice);
+      Console.WriteLine();
 
+      if(choice <= 5)
+      {
+        switch(choice)
+        {
+          case 1:
+            addTask();
+            break;
+          case 2:
+            removeTask();
+            break;
+          case 3:
+            markCompleted();
+            break;
+          case 4:
+            showTasks();
+            break;
+          case 5:
+            isRunning = false;
+            Console.WriteLine("Thank you for using the app! Byebye!");
+            break;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Invalid choice, please try again.");
+      }
+    }
+
+    void addTask()
+    {
+      Console.Clear();
+      Console.WriteLine("addTask function");
+      Console.WriteLine();
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey();
+    }
+
+    void removeTask()
+    {
+      Console.Clear();
+      Console.WriteLine("removeTask function");
+      Console.WriteLine();
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey();
+    }
+
+    void markCompleted()
+    {
+      Console.Clear();
+      Console.WriteLine("markCompleted function");
+      Console.WriteLine();
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey();
+    }
+
+    void showTasks()
+    {
+      Console.Clear();
+      Console.WriteLine("showTasks function");
+      Console.WriteLine();
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey();
+    }
   }
 }
